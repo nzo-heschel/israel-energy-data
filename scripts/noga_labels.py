4d9ab28cab3f538687a08d46fd9f1308d00f7e7d
nzo-heschel <102461987+nzo-heschel@users.noreply.github.com>
2025-06-26 14:12:02 +0300

    change noga label from storage to pumpedStorage [fix #55]

--- scripts/noga_labels.py
+++ scripts/noga_labels.py
@@ -99,7 +99,7 @@ NS_LABEL_POST_MAP = {
         "natural_Gas": "Gas",
         "soler": "Solar",
         "mazut": "Mazut",
-        "storage": "PSP",
+        "pumpedStorage": "PSP",
         "other": "Other",
         "photoVoltaic": "PV",
         "termo_Soler": "Thermo",
